d9eec4b790f5f24d16548065024154af1a2a3b1c
jfdev001 <jfdev001@users.noreply.github.com>
2024-04-24 16:13:41 +0000

    deploy: ad7023f8e9bfcb139887ee865762ca52057b6b80

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['common_2ehpp_0',['common.hpp',['../common_8hpp.html',1,'']]],
-  ['compute_5ferror_5fevery_5fn_5fiters_1',['compute_error_every_n_iters',['../classAMG_1_1SmootherBase.html#a438243f5ab19024bd7ab8be2af2af5df',1,'AMG::SmootherBase']]]
+  ['compat_2ecpp_1',['compat.cpp',['../compat_8cpp.html',1,'']]],
+  ['compute_5ferror_5fevery_5fn_5fiters_2',['compute_error_every_n_iters',['../classAMG_1_1SmootherBase.html#a438243f5ab19024bd7ab8be2af2af5df',1,'AMG::SmootherBase']]]
 ];
