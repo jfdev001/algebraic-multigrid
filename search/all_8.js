c2fa3450c6f659cd2e0f694dafaaf8881033d507
jfdev001 <jfdev001@users.noreply.github.com>
2024-04-25 08:21:40 +0000

    deploy: 32f1838e33e96da84efc6dd849bffa7303f56c01

--- search/all_8.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['mainpage_2emd_0',['mainpage.md',['../mainpage_8md.html',1,'']]],
-  ['make_5foperators_1',['make_operators',['../class_a_m_g_1_1_interpolator_base.html#a1817fd343ff0d32b99987db2034378a1',1,'AMG::InterpolatorBase::make_operators()'],['../class_a_m_g_1_1_linear_interpolator.html#a7b626eaf7c4aabffeac326dd74848d30',1,'AMG::LinearInterpolator::make_operators()']]],
-  ['multigrid_2',['Multigrid',['../class_a_m_g_1_1_multigrid.html',1,'AMG::Multigrid&lt; EleType &gt;'],['../class_a_m_g_1_1_multigrid.html#a97972c7c685d90be62ad8f390260a5cd',1,'AMG::Multigrid::Multigrid()=delete'],['../class_a_m_g_1_1_multigrid.html#aff78df8c3d08f81e6ebf43306ddab4b1',1,'AMG::Multigrid::Multigrid(AMG::InterpolatorBase&lt; EleType &gt; *interpolator_, AMG::SmootherBase&lt; EleType &gt; *smoother_, const Eigen::SparseMatrix&lt; EleType &gt; &amp;A, const Eigen::Matrix&lt; EleType, -1, 1 &gt; &amp;b, size_t n_levels_, EleType tolerance_=1e-9, size_t compute_error_every_n_iters_=10, size_t n_iters_=100)']]],
-  ['multigrid_2ehpp_3',['multigrid.hpp',['../multigrid_8hpp.html',1,'']]]
-];
