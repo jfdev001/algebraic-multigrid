0b0f600ec85eda7ac3a9b6c8a9e4ae6ef10a1a6c
jfdev001 <jfdev001@users.noreply.github.com>
2024-04-25 05:43:07 +0000

    deploy: 0f7912f6c3c7a35c7021f008fd6ec5ce89bea640

---
+++ search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['interpolatorbase_0',['InterpolatorBase',['../classAMG_1_1InterpolatorBase.html#a16aadf883e90026befb33859594dba8c',1,'AMG::InterpolatorBase::InterpolatorBase(size_t n_levels)'],['../classAMG_1_1InterpolatorBase.html#aea611a38b881d22808d233d6c3b48c9d',1,'AMG::InterpolatorBase::InterpolatorBase()']]]
+];
