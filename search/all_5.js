f58eccab947b8449e339e63178afffb8341c6e2f
jfdev001 <jfdev001@users.noreply.github.com>
2024-04-25 07:08:26 +0000

    deploy: 2b92c9cd592b0ff97b58110f879a90e4f38a18dd

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['interpolator_2ehpp_0',['interpolator.hpp',['../interpolator_8hpp.html',1,'']]],
-  ['interpolatorbase_1',['InterpolatorBase',['../class_a_m_g_1_1_interpolator_base.html',1,'AMG::InterpolatorBase&lt; EleType &gt;'],['../class_a_m_g_1_1_interpolator_base.html#a16aadf883e90026befb33859594dba8c',1,'AMG::InterpolatorBase::InterpolatorBase(size_t n_levels)'],['../class_a_m_g_1_1_interpolator_base.html#aea611a38b881d22808d233d6c3b48c9d',1,'AMG::InterpolatorBase::InterpolatorBase()']]]
+  ['interpolatorbase_1',['InterpolatorBase',['../classAMG_1_1InterpolatorBase.html',1,'AMG::InterpolatorBase&lt; EleType &gt;'],['../classAMG_1_1InterpolatorBase.html#a16aadf883e90026befb33859594dba8c',1,'AMG::InterpolatorBase::InterpolatorBase(size_t n_levels)'],['../classAMG_1_1InterpolatorBase.html#aea611a38b881d22808d233d6c3b48c9d',1,'AMG::InterpolatorBase::InterpolatorBase()']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['laplacian_0',['laplacian',['../class_a_m_g_1_1_grid.html#aecbc8db4873c7956891d0ea1bb2c9cf8',1,'AMG::Grid']]]
+  ['laplacian_0',['laplacian',['../classAMG_1_1Grid.html#aecbc8db4873c7956891d0ea1bb2c9cf8',1,'AMG::Grid']]]
 ];
