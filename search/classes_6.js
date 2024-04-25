86604988563c4a154646ed893666b921749e48b3
jfdev001 <jfdev001@users.noreply.github.com>
2024-04-25 08:17:51 +0000

    deploy: 32f1838e33e96da84efc6dd849bffa7303f56c01

--- search/classes_6.js
+++ search/classes_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['smootherbase_0',['SmootherBase',['../classAMG_1_1SmootherBase.html',1,'AMG']]],
-  ['sparsegaussseidel_1',['SparseGaussSeidel',['../classAMG_1_1SparseGaussSeidel.html',1,'AMG']]],
-  ['successiveoverrelaxation_2',['SuccessiveOverRelaxation',['../classAMG_1_1SuccessiveOverRelaxation.html',1,'AMG']]]
+  ['smootherbase_0',['SmootherBase',['../class_a_m_g_1_1_smoother_base.html',1,'AMG']]],
+  ['sparsegaussseidel_1',['SparseGaussSeidel',['../class_a_m_g_1_1_sparse_gauss_seidel.html',1,'AMG']]],
+  ['successiveoverrelaxation_2',['SuccessiveOverRelaxation',['../class_a_m_g_1_1_successive_over_relaxation.html',1,'AMG']]]
 ];
